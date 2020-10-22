c8c04b9ef8f370d0dc5cd6a3279d17c2ac723169
richard_lv <413019339@qq.com>
2020-10-22 21:43:23 +0800

    update leetcode No.20

--- src/main/java/practice/ValidParentheses.java
+++ src/main/java/practice/ValidParentheses.java
@@ -17,9 +17,15 @@ import java.util.Map;
  *
  * 1.Open brackets must be closed by the same type of brackets.
  * 2.Open brackets must be closed in the correct order.
- *
  */
 public class ValidParentheses {
+
+    /**
+     * 首先，构建合法配对括号的key-value map
+     * 创建栈
+     * 遇到左括号，push入栈，遇到右括号，peek栈顶括号查询map是否与之配对，若配对，pop弹出栈顶括号
+     * 完成遍历后，栈中若无元素则输入括号合法，反之不合法
+     */
     public boolean isValid(String s) {
         if (s.length()%2!=0) return false;
         Map<Character,Character> parentheses=new HashMap<>();
@@ -28,15 +34,21 @@ public class ValidParentheses {
         parentheses.put('[',']');
 
         Deque<Character> stack=new LinkedList<>();
+
         for (int i = 0; i < s.length(); i++) {
             char c=s.charAt(i);
             if (!stack.isEmpty()){
+
                 if (parentheses.containsKey(stack.peek())&&c==parentheses.get(stack.peek())){
                     stack.pop();
                 }else{
+                    //可优化1：若push的右括号无法配对栈顶左括号，则该括号后续无论如何也无法将其配对弹出，
+                    //最后栈一定不为空，则输入不合法，可直接return false
                     stack.push(c);
                 }
             }else{
+                //可优化2：当栈为空时，push一个右括号，同上，
+                //最后栈一定不为空，则输入不合法，可直接return false
                 stack.push(c);
             }
         }
